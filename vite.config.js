78eed09991e0ef11933a1afe188d473ea1e19178
ISORegret <rtaylorbrick@gmail.com>
2026-02-04 18:54:10 -0500

    app

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,45 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({ mode }) => ({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg'],
+      manifest: {
+        name: 'SnapMap',
+        short_name: 'SnapMap',
+        description: 'Find and save photography & automotive spots. Map, favorites, best times.',
+        theme_color: '#0a0a0f',
+        background_color: '#0a0a0f',
+        display: 'standalone',
+        start_url: mode === 'android' ? './index.html' : '/',
+        scope: mode === 'android' ? './' : '/',
+        icons: [
+          { src: '/favicon.svg', type: 'image/svg+xml', sizes: 'any', purpose: 'any' },
+          { src: '/favicon.svg', type: 'image/svg+xml', sizes: 'any', purpose: 'maskable' },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,svg,woff2}'],
+        navigateFallback: null,
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/unpkg\.com\/leaflet.*/i,
+            handler: 'CacheFirst',
+            options: { cacheName: 'leaflet-cache', expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 30 } },
+          },
+          {
+            urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: { cacheName: 'google-fonts', expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 } },
+          },
+        ],
+      },
+    }),
+  ],
   base: mode === 'android' ? './' : '/',
   build: {
     outDir: 'dist',
