2b23a7def79f2f69d68a42079744ce7e61617984
ISORegret <rtaylorbrick@gmail.com>
2026-03-23 23:19:32 -0400

    Refactor App and Map components for improved layout and user experience
    - Updated the App component to enhance layout consistency and responsiveness across devices.
    - Improved the Map component's design by refining padding and spacing for better visual balance.
    - Enhanced overall user experience through layout adjustments, ensuring accessibility and aesthetics are prioritized.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-0d7dea0d'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.h536db2869s"
+    "revision": "0.7rl7o16v7es"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
